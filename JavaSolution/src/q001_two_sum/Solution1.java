381e6f513275d6e4cf046621f9b930d79538def5
zhhgit <zhh900601@sina.com>
2017-10-23 17:17:53 +0800

    q001

--- JavaSolution/src/q001_two_sum/Solution1.java
+++ JavaSolution/src/q001_two_sum/Solution1.java
@@ -1,7 +1,32 @@
 package q001_two_sum;
 
+import java.util.HashMap;
+
 /**
  * Created by zhanghao1 on 2017/10/23.
  */
 public class Solution1 {
+    public static void main(String[] args){
+        int[] nums = {3,2,4};
+        int target = 6;
+        for(int num: twoSum(nums,target)){
+            System.out.println(num);
+        }
+    }
+
+    private static int[] twoSum(int[] nums, int target) {
+        int[] ret = new int[2];
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        for(int i = 0;i<nums.length;i++){
+            if(hm.containsKey(target - nums[i])){
+                ret[0] = hm.get(target - nums[i]);
+                ret[1] = i;
+                return ret;
+            }
+            else{
+                hm.put(nums[i], i);
+            }
+        }
+        return ret;
+    }
 }
