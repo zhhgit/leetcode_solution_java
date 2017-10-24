a72aa6ef4eb7bb9fa263d674b515e693e73145d4
zhhgit <zhh900601@sina.com>
2017-10-24 17:28:14 +0800

    q009 and q013

---
+++ JavaSolution/src/q009_palindrome_number/Solution1.java
@@ -0,0 +1,40 @@
+package q009_palindrome_number;
+
+/**
+ * Created by zhanghao1 on 2017/10/24.
+ */
+public class Solution1 {
+    public static void main(String[] args){
+        int[] nums = {5,12,323,44,0,345543,-1};
+        for(int num: nums){
+            System.out.println(isPalindrome(num));
+        }
+    }
+
+    private static boolean isPalindrome(int x) {
+        if(x < 0){
+            return false;
+        }
+        else if(x == 0){
+            return true;
+        }
+        else{
+            return reverseInt((long) x);
+        }
+    }
+
+    private static boolean reverseInt(long x){
+        long original = x;
+        long temp = 0;
+        while(x > 0){
+            temp = temp * 10 + x % 10;
+            x = x / 10;
+        }
+        if(temp == original){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
