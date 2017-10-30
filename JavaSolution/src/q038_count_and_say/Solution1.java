2877271b7a827ec0a456dbf4127e0d4115d1a19a
zhhgit <zhh900601@sina.com>
2017-10-30 10:46:47 +0800

    q035 and q038

--- JavaSolution/src/q038_count_and_say/Solution1.java
+++ JavaSolution/src/q038_count_and_say/Solution1.java
@@ -5,10 +5,38 @@ package q038_count_and_say;
  */
 public class Solution1 {
     public static void main(String[] args){
-
+        int[] nArr = {1,2,3,4,5};
+        for(int n:nArr){
+            System.out.println(countAndSay(n));
+        }
     }
 
     private static String countAndSay(int n) {
-        return "a";
+        if(n == 1){
+            return "1";
+        }
+        else if(n == 2){
+            return "11";
+        }
+        else{
+            StringBuffer sb = new StringBuffer();
+            String last = countAndSay(n-1);
+            char temp = last.charAt(0);
+            int tempCount = 1;
+            for(int i = 1;i<last.length();i++){
+                if(last.charAt(i) == temp){
+                    tempCount++;
+                }
+                else{
+                    sb.append(tempCount);
+                    sb.append(temp);
+                    temp = last.charAt(i);
+                    tempCount = 1;
+                }
+            }
+            sb.append(tempCount);
+            sb.append(temp);
+            return sb.toString();
+        }
     }
 }
