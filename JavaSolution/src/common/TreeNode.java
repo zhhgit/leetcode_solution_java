adccfdda7ebd231348a04c46a8b553a5428758ab
zhhgit <zhh900601@sina.com>
2017-11-02 11:31:18 +0800

    add TreeNode

---
+++ JavaSolution/src/common/TreeNode.java
@@ -0,0 +1,13 @@
+package common;
+
+/**
+ * Created by zhanghao1 on 2017/11/2.
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int x){
+        val = x;
+    }
+}
