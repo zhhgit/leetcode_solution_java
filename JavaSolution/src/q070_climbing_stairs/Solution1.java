f9538ec475d7716f1d8715483b80cc1c970acc98
zhhgit <zhh900601@sina.com>
2017-11-02 10:59:39 +0800

    q070

---
+++ JavaSolution/src/q070_climbing_stairs/Solution1.java
@@ -0,0 +1,39 @@
+package q070_climbing_stairs;
+
+/**
+ * Created by zhanghao1 on 2017/11/1.
+ */
+public class Solution1 {
+    public static void main(String[] args){
+        int n = 6;
+        System.out.println(climbStairs(n));
+    }
+
+    private static int climbStairs(int n) {
+        if(n<=0){
+            return 0;
+        }
+        else if(n==1){
+            return 1;
+        }
+        else if(n==2){
+            return 2;
+        }
+        int[] resultArr = new int[n + 1];
+        resultArr[0] = 0;
+        resultArr[1] = 1;
+        resultArr[2] = 2;
+        return recursiveClimb(n,resultArr);
+    }
+
+    private static int recursiveClimb(int n,int[] resultArr){
+        if(resultArr[n] != 0){
+            return resultArr[n];
+        }
+        else{
+            int result = recursiveClimb(n-2,resultArr) + recursiveClimb(n-1,resultArr);
+            resultArr[n] = result;
+            return result;
+        }
+    }
+}
