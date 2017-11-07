4197381385c1d83c78277f2d5ada93cb1424f118
zhhgit <zhh900601@sina.com>
2017-11-07 12:48:43 +0800

    q141

---
+++ JavaSolution/src/q141_linked_list_cycle/Solution1.java
@@ -0,0 +1,33 @@
+package q141_linked_list_cycle;
+
+import common.ListBuilder;
+import common.ListNode;
+import common.TreeBuilder;
+
+/**
+ * Created by zhanghao1 on 2017/11/7.
+ */
+public class Solution1 {
+    public static void main(String[] args){
+        int[] nums = {1,2,3,4,5,6,7,8,9};
+        ListBuilder lb = new ListBuilder(nums);
+        System.out.println(hasCycle(lb.getHead()));
+    }
+
+    private static boolean hasCycle(ListNode head) {
+        if(head == null){
+            return false;
+        }
+        ListNode slow = head,fast = head.next;
+        while (slow.next != null && fast.next != null && fast.next.next != null && slow != fast){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        if(slow == fast){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
