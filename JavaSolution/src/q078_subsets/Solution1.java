59fc290ea31ff2564aff54b4df5c12133a55a487
zhhgit <zhh900601@sina.com>
2017-11-24 09:00:49 +0800

    78

---
+++ JavaSolution/src/q078_subsets/Solution1.java
@@ -0,0 +1,48 @@
+package q078_subsets;
+
+import org.omg.PortableInterceptor.INACTIVE;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Solution1 {
+    public static void main(String[] args){
+        int[] nums = {1,2,3};
+        System.out.println(subsets(nums));
+    }
+
+    private static List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> lists = new LinkedList<>();
+        if(nums.length == 0){
+            return lists;
+        }
+        List<Integer> tempList = new LinkedList<>();
+        boolean[] used = new boolean[nums.length];
+        backTracking(nums,lists,tempList,used);
+        return lists;
+    }
+
+    private static void backTracking(int[] nums,List<List<Integer>> lists,List<Integer> tempList, boolean[] used){
+        List<Integer> addList = new LinkedList<>(tempList);
+        lists.add(addList);
+        if(tempList.size() < nums.length){
+            int prev;
+            if(tempList.size() > 0){
+                prev = tempList.get(tempList.size()-1);
+            }
+            else{
+                prev = Integer.MIN_VALUE;
+            }
+            for(int i = 0;i<nums.length;i++){
+
+                if(nums[i]>prev && !used[i]){
+                    tempList.add(nums[i]);
+                    used[i] = true;
+                    backTracking(nums,lists,tempList,used);
+                    tempList.remove(tempList.size()-1);
+                    used[i] = false;
+                }
+            }
+        }
+    }
+}
