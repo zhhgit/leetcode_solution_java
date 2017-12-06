ad5be25c4edf6cab4c27a4e2e862aa332ecd7948
zhhgit <zhh900601@sina.com>
2017-12-06 18:40:22 +0800

    86

---
+++ JavaSolution/src/q086_partition_list/Solution1.java
@@ -0,0 +1,41 @@
+package q086_partition_list;
+
+import common.ListBuilder;
+import common.ListNode;
+import common.PrintUtil;
+
+import java.util.LinkedList;
+
+public class Solution1 {
+    public static void main(String[] args){
+        int[] nums = {1,4,3,2,5,2};
+        int x = 3;
+        ListBuilder listBuilder = new ListBuilder(nums);
+        PrintUtil.printList(partition(listBuilder.getHead(),x));
+    }
+
+    private static ListNode partition(ListNode head, int x) {
+        if(head == null){
+            return null;
+        }
+        ListNode fakeHead = new ListNode(0);
+        ListNode curr = fakeHead;
+        ListNode pointer = head;
+        while (pointer != null){
+            if(pointer.val < x){
+                curr.next = new ListNode(pointer.val);
+                curr = curr.next;
+            }
+            pointer = pointer.next;
+        }
+        pointer = head;
+        while (pointer != null){
+            if(pointer.val >= x){
+                curr.next = new ListNode(pointer.val);
+                curr = curr.next;
+            }
+            pointer = pointer.next;
+        }
+        return fakeHead.next;
+    }
+}
