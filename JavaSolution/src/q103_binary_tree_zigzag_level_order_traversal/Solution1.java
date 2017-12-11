d5e2e767dfad779ffec3eee9d0672b2d12a5e2d7
zhhgit <zhh900601@sina.com>
2017-12-11 17:12:41 +0800

    103

---
+++ JavaSolution/src/q103_binary_tree_zigzag_level_order_traversal/Solution1.java
@@ -0,0 +1,55 @@
+package q103_binary_tree_zigzag_level_order_traversal;
+
+import common.TreeBuilder;
+import common.TreeNode;
+import org.omg.Messaging.SYNC_WITH_TRANSPORT;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+public class Solution1 {
+    public static void main(String[] args){
+        int[] nums = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+        TreeBuilder tb = new TreeBuilder(nums);
+        System.out.println(zigzagLevelOrder(tb.getRoot()));
+    }
+
+    private static List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> lists = new LinkedList<>();
+        if(root == null){
+            return lists;
+        }
+        Stack<TreeNode> currStack = new Stack<>();
+        currStack.push(root);
+        boolean flag = true;
+        while (currStack.size()!=0){
+            Stack<TreeNode> nextStack = new Stack<>();
+            List<Integer> addList = new LinkedList<>();
+            while(currStack.size()!=0){
+                TreeNode currNode = currStack.pop();
+                addList.add(currNode.val);
+                if(flag){
+                    if(currNode.left != null){
+                        nextStack.push(currNode.left);
+                    }
+                    if(currNode.right != null){
+                        nextStack.push(currNode.right);
+                    }
+                }
+                else{
+                    if(currNode.right != null){
+                        nextStack.push(currNode.right);
+                    }
+                    if(currNode.left != null){
+                        nextStack.push(currNode.left);
+                    }
+                }
+            }
+            lists.add(addList);
+            currStack = nextStack;
+            flag = !flag;
+        }
+        return lists;
+    }
+}
