d201588db321cac0ea38dde67d7bf79e166db5b5
zhhgit <zhh900601@sina.com>
2017-12-21 08:37:15 +0800

    203

---
+++ JavaSolution/src/q203_remove_linked_list_elements/Solution1.java
@@ -0,0 +1,32 @@
+package q203_remove_linked_list_elements;
+
+import common.ListBuilder;
+import common.ListNode;
+import common.PrintUtil;
+
+public class Solution1 {
+    public static void main(String[] args){
+        int val = 1;
+        int[] nums = {1,2,3,4,1,3,4};
+        ListBuilder listBuilder = new ListBuilder(nums);
+        PrintUtil.printList(removeElements(listBuilder.getHead(),val));
+    }
+
+    private static ListNode removeElements(ListNode head, int val) {
+        if(head == null){
+            return null;
+        }
+        ListNode fakeHead = new ListNode(0);
+        fakeHead.next = head;
+        ListNode pointer = fakeHead;
+        while(pointer != null && pointer.next != null){
+            if(pointer.next.val == val){
+                pointer.next = pointer.next.next;
+            }
+            else{
+                pointer = pointer.next;
+            }
+        }
+        return fakeHead.next;
+    }
+}
