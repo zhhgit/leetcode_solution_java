71bec6ed4c761e446e6e7420251bc7ca0c7ba7f2
zhhgit <zhh900601@sina.com>
2018-01-04 09:26:04 +0800

    130

--- JavaSolution/src/q130_surrounded_regions/Solution1.java
+++ JavaSolution/src/q130_surrounded_regions/Solution1.java
@@ -1,8 +1,5 @@
 package q130_surrounded_regions;
 
-import java.util.LinkedList;
-import java.util.List;
-
 public class Solution1 {
 
     public static void main(String[] args){
@@ -21,6 +18,49 @@ public class Solution1 {
         int columnLen = board[0].length;
         if(columnLen == 0){
             return;
+        }
+        for(int i=0;i<columnLen;i++){
+            backTracking(board,0,i);
+            backTracking(board,rowLen-1,i);
+        }
+        for(int i=0;i<rowLen;i++){
+            backTracking(board,i,0);
+            backTracking(board,i,columnLen-1);
+        }
+        changeAll(board);
+    }
+
+    //检验(x,y)位置，并递归其四周
+    private static void backTracking(char[][] board,int x,int y){
+        int rowLen = board.length;
+        int columnLen = board[0].length;
+        if(x>=0 && y>=0 && x<rowLen && y<columnLen){
+            if(board[x][y] == 'O'){
+                board[x][y] = '*';
+                backTracking(board,x-1,y);
+                backTracking(board,x+1,y);
+                backTracking(board,x,y-1);
+                backTracking(board,x,y+1);
+            }
+        }
+    }
+
+    private static void changeAll(char[][] board){
+        int rowLen = board.length;
+        int columnLen = board[0].length;
+        for(int i=0;i<rowLen;i++){
+            for(int j=0;j<columnLen;j++){
+                if(board[i][j] == 'O'){
+                    board[i][j] = 'X';
+                }
+            }
+        }
+        for(int i=0;i<rowLen;i++){
+            for(int j=0;j<columnLen;j++){
+                if(board[i][j] == '*'){
+                    board[i][j] = 'O';
+                }
+            }
         }
     }
 
