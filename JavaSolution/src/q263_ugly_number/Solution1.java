c540f7efd088ab42b04b803542cf266591b88aa3
zhhgit <zhh900601@sina.com>
2018-01-31 18:54:41 +0800

    263

---
+++ JavaSolution/src/q263_ugly_number/Solution1.java
@@ -0,0 +1,29 @@
+package q263_ugly_number;
+
+public class Solution1 {
+    public static void main(String[] args){
+        int[] nums = {3,5,6,8,21,12,10};
+        for(int num:nums){
+            System.out.println(isUgly(num));
+        }
+    }
+
+    private static boolean isUgly(int num) {
+        if(num <= 0){
+            return false;
+        }
+        if(num == 1){
+            return true;
+        }
+        if(num % 2 == 0){
+            return isUgly(num / 2);
+        }
+        if(num % 3 == 0){
+            return isUgly(num / 3);
+        }
+        if(num % 5 == 0){
+            return isUgly(num / 5);
+        }
+        return false;
+    }
+}
