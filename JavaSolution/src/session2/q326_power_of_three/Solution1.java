08dcec5aa9e132f8b73554b556a690b7a9c7719a
zhhgit <zhh900601@sina.com>
2018-03-23 16:05:38 +0800

    move to session2

---
+++ JavaSolution/src/session2/q326_power_of_three/Solution1.java
@@ -0,0 +1,21 @@
+package session2.q326_power_of_three;
+
+public class Solution1 {
+	public static void main(String[] args){
+		System.out.println(isPowerOfThree(1));
+		System.out.println(isPowerOfThree(2));
+		System.out.println(isPowerOfThree(3));
+		System.out.println(isPowerOfThree(27));
+		System.out.println(isPowerOfThree(29));
+		System.out.println(isPowerOfThree(9));
+	}
+	
+    private static boolean isPowerOfThree(int n) {
+        long max3PowerLong = 1;
+        while(max3PowerLong * 3 < Integer.MAX_VALUE){
+        	max3PowerLong *= 3;
+        }
+        int max = (int) max3PowerLong;
+        return (n > 0 && max % n == 0);
+    }
+}
---
+++ JavaSolution/src/session2/q367_valid_perfect_square/Solution1.java
@@ -0,0 +1,19 @@
+package session2.q367_valid_perfect_square;
+
+public class Solution1 {
+	public static void main(String[] args) {
+		int[] nums = {1,16,14};
+		for(int num:nums) {
+			System.out.println(isPerfectSquare(num));	
+		}
+	}
+	
+    private static boolean isPerfectSquare(int num) {
+    	if(num < 0) {
+    		return false;
+    	}
+    	String[] stringArr = (Math.sqrt(num) + "").split("\\.");
+    	int sqrtResult = Integer.parseInt(stringArr[0]);
+    	return (sqrtResult * sqrtResult == num);
+    }
+}
