e740bd137f7f410df6ea87cb4f3609e5168adcbb
zhhgit <zhh900601@sina.com>
2018-04-24 18:32:10 +0800

    15

---
+++ JavaSolution/src/session3/q015_3sum/Solution1.java
@@ -0,0 +1,16 @@
+package session3.q015_3sum;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Solution1 {
+    public static void main(String[] args){
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        System.out.println(threeSum(nums));
+    }
+
+    private static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> lists = new LinkedList<>();
+        return lists;
+    }
+}
